7ec16bc3e9b5a506414ef7101df998dff696b5b1
wmwaqas <wmwaqasmahmood@hotmail.com>
2017-08-09 12:56:28 +0200

    New Project with Tests and Props

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Title from './components/Title'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Title />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+class Title extends PureComponent {
+  render() {
+    return(
+      <h1>{ this.props.content }</h1>
+    )
+  }
+}
+
+export default Title
